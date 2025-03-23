6137e7c8b7f8fea614e1d8cd4a0fd4ff1d6591c6
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-03-23 05:57:40 +0200

    setup supabase for backend & database services and creating tables & buckets

---
+++ src/services/apiCabins.js
@@ -0,0 +1,19 @@
+import supabase from "./supabase";
+
+
+export async function getCabins() {
+
+    const { data, error } = await supabase
+        .from('cabins')
+        .select('*');
+
+    if (error) {
+        console.error(error);
+        throw new Error('Cabins could not be loaded');
+    }
+    console.log(data);
+
+    return data;
+
+}
+
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ppmizsdaagwxepjukjgx.supabase.co';
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwbWl6c2RhYWd3eGVwanVramd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1Mjk2NzgsImV4cCI6MjA1ODEwNTY3OH0.xggFZSKK1_mwLpvRWVFourM4ZNWkZfacAD9o-OUYOus`;
+const supabase = createClient(supabaseUrl, supabaseKey, { global: { fetch: fetch.bind(globalThis) } });
+
+export default supabase;
