7a5d0229bf779ced0aaf2f131e8e7a715d307b70
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-03-28 04:24:48 +0200

    creating new cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -15,6 +15,21 @@ export async function getCabins() {
     return data;
 }
 
+export async function createCabin(newCabin) {
+
+    const { data, error } = await supabase
+        .from('cabins')
+        .insert([newCabin])
+        .select();
+
+    if (error) {
+        console.error(error);
+        throw new Error('Cabin could not be deleted');
+    }
+
+    return data;
+}
+
 export async function deleteCabin(id) {
 
     const { data, error } = await supabase
