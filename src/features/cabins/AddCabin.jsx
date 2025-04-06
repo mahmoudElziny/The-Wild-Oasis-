0b6f542559d685fc9cb92089f21957ec99d08df5
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-04-06 15:36:13 +0200

    confirming cabin deletions

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -2,24 +2,18 @@
 import CreateCabinForm from "./CreateCabinForm";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
-import CabinTable from "./CabinTable";
 
 function AddCabin() {
-    return <Modal>
-        <Modal.Open opens='cabin-form'>
-            <Button>Add new cabin</Button>
-        </Modal.Open>
-        <Modal.Window name='cabin-form'>
-            <CreateCabinForm />
-        </Modal.Window>
-
-        <Modal.Open opens='table'>
-            <Button>Show cabins table</Button>
-        </Modal.Open>
-        <Modal.Window name='table'>
-            <CabinTable />
-        </Modal.Window>
-    </Modal>
+    return <div>
+        <Modal>
+            <Modal.Open opens='cabin-form'>
+                <Button>Add new cabin</Button>
+            </Modal.Open>
+            <Modal.Window name='cabin-form'>
+                <CreateCabinForm />
+            </Modal.Window>
+        </Modal>
+    </div>
 }
 // export default function AddCabin() {
 
