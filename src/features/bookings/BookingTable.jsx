fc3b929d1c85850b3b4a1d5662fe97e5bab969a2
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-04-19 17:52:54 +0200

    Error Boundaries

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -11,6 +11,7 @@ function BookingTable() {
   const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) return <Spinner />;
+
   if (!bookings.length) return <Empty resourceName="bookings" />
 
   return (
